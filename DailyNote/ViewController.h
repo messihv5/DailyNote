39c2e47f0bb654580d8f21f8a90e41802997ef9b
Messi <messi@lionel-messi.local>
2016-05-24 08:01:20 +0800

    Initial Commit

---
+++ DailyNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyNote
+//
+//  Created by Messi on 16/5/24.
+//  Copyright © 2016年 Messi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
