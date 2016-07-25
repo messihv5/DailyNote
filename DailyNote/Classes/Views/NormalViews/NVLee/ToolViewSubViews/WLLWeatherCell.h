5ca61b8830a1d6811463af7c9dda46dc05cb0d8f
CapExample <bichao_li@yahoo.com>
2016-07-25 22:56:34 +0800

    添加WLLWeatherCell

---
+++ DailyNote/Classes/Views/NormalViews/NVLee/ToolViewSubViews/WLLWeatherCell.h
@@ -0,0 +1,15 @@
+//
+//  WLLWeatherCell.h
+//  DailyNote
+//
+//  Created by CapLee on 16/7/25.
+//  Copyright © 2016年 Messi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLLWeatherCell : UICollectionViewCell
+
+@property (nonatomic, strong) UIImageView *weatherIcon;
+
+@end
